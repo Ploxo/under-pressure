65e5fb17086104d03ed0251d9d412495f82c4f97
Digleer <106885672+Digleer@users.noreply.github.com>
2023-01-05 17:29:37 +0100

    Button changes, added example sound

--- under-pressure/Assets/Scripts/MainButton.cs
+++ under-pressure/Assets/Scripts/MainButton.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 
-public class Button : MonoBehaviour
+// Main button for the ship
+public class MainButton : MonoBehaviour
 {
     [SerializeField] Light selfLight;
     [SerializeField] ButtonInteractable interactable;
@@ -14,16 +14,24 @@ public class Button : MonoBehaviour
         SetButtonReady(true);
     }
 
-    // If true, will allow the interactable to interact and fire events; if false it is static
+    private void OnEnable()
+    {
+        interactable.OnPress.AddListener(Activate);
+    }
+
+    private void OnDisable()
+    {
+        interactable.OnPress.RemoveListener(Activate);
+    }
+
     public void SetButtonReady(bool value)
     {
         selfLight.enabled = value;
         interactable.enabled = value;
     }
 
-    // Triggered by the interactable press event
     public void Activate()
     {
-        
+        selfLight.enabled = false;
     }
 }
